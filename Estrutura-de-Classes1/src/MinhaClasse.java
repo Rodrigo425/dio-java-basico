ef470ad8c69be70cd05c0528809868b6b1be4950
Rodrigo <rodrigoxbox370@gmail.com>
2023-03-04 18:15:33 -0300

    Estrutura-de-Classes

---
+++ Estrutura-de-Classes1/src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse {
+    //Estrutura padrão da classe dentro de () é o parametro 
+    //Classe system
+   public static void main(String[] args){     //Método main
+    System.out.println("Olá mundo");          //Método
+   }                      
+}
